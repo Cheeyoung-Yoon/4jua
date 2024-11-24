a0714f9e800b20d6f354d8f69ce677c62b99b621
is_ai <61747619+Cheeyoung-Yoon@users.noreply.github.com>
2024-11-24 17:49:51 +0000

    test

--- temp_folder/gallery/upload/page.tsx
+++ temp_folder/gallery/upload/page.tsx
@@ -1,7 +1,7 @@
 // app/upload/page.tsx
 'use client'
 import { useState } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../../jua-app/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function Upload() {
